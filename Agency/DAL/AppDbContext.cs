af00009f9e81189e2c5a23eefe02efbfb33cce1c
T x <emraslanovsupan290@gmail.com>
2024-01-07 20:02:42 +0400

    Added Settings

--- Agency/DAL/AppDbContext.cs
+++ Agency/DAL/AppDbContext.cs
@@ -10,5 +10,7 @@ namespace Agency.DAL
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Settings> Settings { get; set; }
+
     }
 }
---
+++ Agency/Models/Settings.cs
@@ -0,0 +1,10 @@
+﻿using Agency.Models.Common;
+
+namespace Agency.Models
+{
+    public class Settings : BaseEntity
+    {
+        public string Key { get; set; } = null!;
+        public string Value { get; set; } = null!;
+    }
+}
